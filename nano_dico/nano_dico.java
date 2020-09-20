066083039a7f38d29736fdd83aef822a25d0835b
idembele70 <idembele70@gmail.com>
2020-09-20 13:06:20 +0200

    nano dico V2 avec des ameliorations personnelle

--- nano_dico/nano_dico.java
+++ nano_dico/nano_dico.java
@@ -1,26 +1,39 @@
+
 /**
  * nano_dico
  */
 import java.util.Scanner;
 import java.util.Arrays;
+
 public class nano_dico {
     public static void main(String[] args) {
+        final String APP_TITLE = "------------\n" + "NANO DICO" + "\n------------";
+        final String QUESTION = "\nVoulez-vous recommencer ?";
+        final String END_MESSAGE = "Au revoir ! :)";
+        String userReponse = "";
+        System.out.println(APP_TITLE);
+        Scanner scListeSaisie = new Scanner(System.in);
 
-        Scanner scListeSaisie = new Scanner(System.in) ;
-        System.out.println("Ecrivez la liste de mot à trié");
-        String text = scListeSaisie.nextLine();
-        
-        String[] userListe = text.split(" "); 
-        Arrays.sort(userListe);
-        System.out.println(Arrays.toString(userListe)); 
+        // MAIN Code
+        while (!userReponse.equals("non")) {
+            try {
+                System.out.println("Ecrivez la liste de mot à trié");
+                String text = scListeSaisie.nextLine();
+                String[] userListe = text.split(" ");
+                Arrays.sort(userListe);
+                System.out.println(Arrays.toString(userListe));
 
+                System.out.println(QUESTION);
+                userReponse = scListeSaisie.nextLine();
+            } catch (Exception exception) {
+                if (scListeSaisie.hasNextLine()) {
+                    scListeSaisie.nextLine();
+                }
+            }
+        }
+        System.out.println(END_MESSAGE);
         scListeSaisie.close();
-  
 
+    }
 
-
-    
-}
-
-    
 }
